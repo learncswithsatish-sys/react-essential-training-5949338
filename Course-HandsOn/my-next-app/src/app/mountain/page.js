71cc34323e6f85407bc37436dd260f9f65f5cf81
learncswithsatish-sys <learncswithsatish@gmail.com>
2026-06-07 17:56:39 +0000

    Link, app router in next js

---
+++ Course-HandsOn/my-next-app/src/app/mountain/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+   <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+       <h1> Hello ...</h1>
+      </main>
+  )
+}
