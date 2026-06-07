132b7833dc1c130133dce7f75ccc3bfc2e070d9a
learncswithsatish-sys <learncswithsatish@gmail.com>
2026-06-07 12:16:35 +0000

    Added new codes

--- Course-HandsOn/react-project/src/App.jsx
+++ Course-HandsOn/react-project/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 
 let launguage = "JavaScript";
 let moon = "🌕";
@@ -7,22 +8,19 @@ function App() {
   return (
     <div>
       <TimeOfDayGreeting />
-      <WelcomeMessage launguage={launguage} moon={moon} />
-      <WelcomeMessage launguage="Java" moon="mars" />{" "}
-      {/* passing different props to demonstrate reusability of the WelcomeMessage component */}
-      <PropsDemo
-        developerInfo={{
-          name: "Satish",
-          level: "Beginner",
-          knowlang: "JavaScript",
-        }}
+      <WelcomeMessage
+        preRequisites={["HTML", "CSS"]}
+        launguage={launguage}
+        moon={moon}
       />
+      {/* passing different props to demonstrate reusability of the WelcomeMessage component */}
+
       <CodingSkillsInfo skillsInfo={codingSkills} />
     </div>
   );
 }
 
-function WelcomeMessage({ launguage, moon }) {
+function WelcomeMessage({ preRequisites, launguage, moon }) {
   console.log(
     "Rendering WelcomeMessage with launguage:",
     launguage,
@@ -33,31 +31,48 @@ function WelcomeMessage({ launguage, moon }) {
     <div>
       <h2>
         {" "}
-        <marquee scrollamount="20" style={{ fontSize: "24px" }}>
-          Hello, {launguage} developers! on the planet {moon}
-        </marquee>
+        Hello, {launguage} developers! on the planet {moon}
       </h2>
       <p>
         Welcome to the React course. Let's build something amazing together!
       </p>
+      <PropsDemo
+        developerInfo={{
+          name: "Satish",
+          level: "Beginner",
+          knowlang: "JavaScript",
+          preRequisites: preRequisites,
+        }}
+      />
     </div>
   );
 }
 
 function PropsDemo({ developerInfo }) {
-  const { name, level, knowlang } = developerInfo;
-  console.log("Received props in PropsDemo:", { name, level, knowlang });
+  const { name, level, knowlang, preRequisites } = developerInfo;
+  // Using useState to demonstrate state management in React. The output will show the initial state value and the function to update it.
+  const [competencyLevel, updateCompetencyLevel] = useState(level);
+  console.log("useState output:", competencyLevel);
+  console.log("Received props in PropsDemo:", {
+    name,
+    level,
+    knowlang,
+    preRequisites,
+  });
   return (
     <div className="props-demo">
       <p>
-        Hi <b>{name}</b>!
+        Developer Name: <b>{name}</b>!
       </p>
       <p>
-        You are at level: <strong>{level}</strong>
+        You are at level: <strong>{competencyLevel}</strong> for React!
       </p>
       <p>
-        You know: <em>{knowlang}</em>
+        Pre-requisites Skills: <em>{preRequisites.join(", ")}</em>
       </p>
+      <button onClick={() => updateCompetencyLevel("Intermediate")}>
+        Update Competency Level
+      </button>
     </div>
   );
 }
